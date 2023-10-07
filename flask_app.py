515680a0f431aa2fbf8ddac842898e687b986ed1
karambir-gahlot <karambir2522@gmail.com>
2023-10-06 22:39:12 -0400

    Add homepage

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Space Apps!'
+    return render_template('main.html')
 
